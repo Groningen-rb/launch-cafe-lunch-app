67c67d8711dde26003769840799b2a219711eba9
Ivan Malykh <ivan@jet-stream.nl>
2013-03-19 22:49:07 +0100

    added email field

--- app/controllers/lunch_controller.rb
+++ app/controllers/lunch_controller.rb
@@ -1,14 +1,20 @@
-class LunchViewController < UIViewController
+class LunchController < UIViewController
 
   def viewDidLoad
     super
 
+    width = Device.screen.width_for_orientation(Device.orientation)
+
     self.view.backgroundColor = '#2988e1'.to_color
 
-    @email = UITextField.alloc.initWithFrame(CGRectMake(0, 80, width, 79))
+    @email = UITextField.alloc.initWithFrame(CGRectMake(30, 30, width - 60, 30))
     @email.placeholder = 'Email'
     @email.tag = 1
-    @email.font = UIFont.fontWithName("Avenir Next", size: 40)
+    @email.font = UIFont.fontWithName("Avenir Next", size: 24)
+    @email.backgroundColor = '#ffffff'.to_color
+    @email.layer.cornerRadius = 3
+    @email.layer.masksToBounds = true
+    @email.keyboardType = UIKeyboardTypeEmailAddress
 
     view.addSubview @email
   end
