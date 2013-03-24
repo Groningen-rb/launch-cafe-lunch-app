f5464d2fccf753390ac9cd4529c5835b36cf087a
Ivan Malykh <ivan@jet-stream.nl>
2013-03-24 17:40:06 +0100

    also tf launcher

--- app/testflight_launcher.rb
+++ app/testflight_launcher.rb
@@ -2,6 +2,6 @@
 
 if Object.const_defined?('TestFlight') and !UIDevice.currentDevice.model.include?('Simulator')
   NSNotificationCenter.defaultCenter.addObserverForName(UIApplicationDidBecomeActiveNotification, object:nil, queue:nil, usingBlock:lambda do |notification|
-  TestFlight.takeOff('ENV["TF_TEAM_TOKEN"]')
+  TestFlight.takeOff('44af1ad12b490762ac48990d7f8343b4_MjAzMjA5MjAxMy0wMy0yNCAxMTo0OTozNS4yMzQzOTM')
   end)
 end
